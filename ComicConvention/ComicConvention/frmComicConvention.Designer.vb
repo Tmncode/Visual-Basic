7ea967cd25e95984ed96bc1a2b95fce3e1ed5cb6
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:42:35 -0600

    Add files via upload

---
+++ ComicConvention/ComicConvention/frmComicConvention.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmComicConvention
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblGroupSize = New System.Windows.Forms.Label()
+        Me.txtGroupSize = New System.Windows.Forms.TextBox()
+        Me.grpBadge = New System.Windows.Forms.GroupBox()
+        Me.radConvention = New System.Windows.Forms.RadioButton()
+        Me.radAutoGraphs = New System.Windows.Forms.RadioButton()
+        Me.radSuperHero = New System.Windows.Forms.RadioButton()
+        Me.lblRegistrationCost = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpBadge.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.ComicConvention.My.Resources.Resources.comic
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1080, 192)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.9!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblHeading.Location = New System.Drawing.Point(154, 207)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(778, 61)
+        Me.lblHeading.TabIndex = 1
+        Me.lblHeading.Text = "Comic Convention Registration"
+        '
+        'lblGroupSize
+        '
+        Me.lblGroupSize.AutoSize = True
+        Me.lblGroupSize.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGroupSize.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblGroupSize.Location = New System.Drawing.Point(313, 299)
+        Me.lblGroupSize.Name = "lblGroupSize"
+        Me.lblGroupSize.Size = New System.Drawing.Size(242, 46)
+        Me.lblGroupSize.TabIndex = 2
+        Me.lblGroupSize.Text = "Group Size:"
+        '
+        'txtGroupSize
+        '
+        Me.txtGroupSize.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtGroupSize.Location = New System.Drawing.Point(648, 296)
+        Me.txtGroupSize.Name = "txtGroupSize"
+        Me.txtGroupSize.Size = New System.Drawing.Size(100, 53)
+        Me.txtGroupSize.TabIndex = 3
+        Me.txtGroupSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'grpBadge
+        '
+        Me.grpBadge.Controls.Add(Me.radConvention)
+        Me.grpBadge.Controls.Add(Me.radAutoGraphs)
+        Me.grpBadge.Controls.Add(Me.radSuperHero)
+        Me.grpBadge.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpBadge.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.grpBadge.Location = New System.Drawing.Point(149, 366)
+        Me.grpBadge.Name = "grpBadge"
+        Me.grpBadge.Size = New System.Drawing.Size(807, 223)
+        Me.grpBadge.TabIndex = 4
+        Me.grpBadge.TabStop = False
+        Me.grpBadge.Text = "Select Badge Type:"
+        '
+        'radConvention
+        '
+        Me.radConvention.AutoSize = True
+        Me.radConvention.Location = New System.Drawing.Point(7, 160)
+        Me.radConvention.Name = "radConvention"
+        Me.radConvention.Size = New System.Drawing.Size(237, 43)
+        Me.radConvention.TabIndex = 2
+        Me.radConvention.TabStop = True
+        Me.radConvention.Text = "Convention"
+        Me.radConvention.UseVisualStyleBackColor = True
+        '
+        'radAutoGraphs
+        '
+        Me.radAutoGraphs.AutoSize = True
+        Me.radAutoGraphs.Location = New System.Drawing.Point(7, 102)
+        Me.radAutoGraphs.Name = "radAutoGraphs"
+        Me.radAutoGraphs.Size = New System.Drawing.Size(462, 43)
+        Me.radAutoGraphs.TabIndex = 1
+        Me.radAutoGraphs.TabStop = True
+        Me.radAutoGraphs.Text = "Convention + Autographs"
+        Me.radAutoGraphs.UseVisualStyleBackColor = True
+        '
+        'radSuperHero
+        '
+        Me.radSuperHero.AutoSize = True
+        Me.radSuperHero.Location = New System.Drawing.Point(7, 45)
+        Me.radSuperHero.Name = "radSuperHero"
+        Me.radSuperHero.Size = New System.Drawing.Size(633, 43)
+        Me.radSuperHero.TabIndex = 0
+        Me.radSuperHero.TabStop = True
+        Me.radSuperHero.Text = "Convention + Superhero experience"
+        Me.radSuperHero.UseVisualStyleBackColor = True
+        '
+        'lblRegistrationCost
+        '
+        Me.lblRegistrationCost.AutoSize = True
+        Me.lblRegistrationCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRegistrationCost.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblRegistrationCost.Location = New System.Drawing.Point(110, 630)
+        Me.lblRegistrationCost.Name = "lblRegistrationCost"
+        Me.lblRegistrationCost.Size = New System.Drawing.Size(305, 39)
+        Me.lblRegistrationCost.TabIndex = 5
+        Me.lblRegistrationCost.Text = "Registration Cost:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnCalculate.Location = New System.Drawing.Point(117, 721)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(229, 84)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnClear.Location = New System.Drawing.Point(441, 721)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(229, 84)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Refresh"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnExit.Location = New System.Drawing.Point(765, 721)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(229, 84)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmComicConvention
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1104, 885)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblRegistrationCost)
+        Me.Controls.Add(Me.grpBadge)
+        Me.Controls.Add(Me.txtGroupSize)
+        Me.Controls.Add(Me.lblGroupSize)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmComicConvention"
+        Me.Text = "Comic Convention Registration"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpBadge.ResumeLayout(False)
+        Me.grpBadge.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblGroupSize As Label
+    Friend WithEvents txtGroupSize As TextBox
+    Friend WithEvents grpBadge As GroupBox
+    Friend WithEvents radConvention As RadioButton
+    Friend WithEvents radAutoGraphs As RadioButton
+    Friend WithEvents radSuperHero As RadioButton
+    Friend WithEvents lblRegistrationCost As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ ComicConvention/ComicConvention/frmComicConvention.vb
@@ -0,0 +1,82 @@
+﻿'Program Name: Comic Convention
+'Author:       Tam M. Nguyen
+'Date:         May 27,2020
+'Purpose:      This Classic Window App computes the registration cost of attending 
+'              a Comic Convention
+
+Option Strict On
+
+Public Class frmComicConvention
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'This event handler calculate the cost per-person and the type of convention
+
+        'Creating variable
+        Dim decGroupSize As Decimal 'Hold the text value 
+        Dim decGroupCost As Decimal 'Hold the cost per-person and convention type
+        Dim decTotalCost As Decimal 'Is assigned and calculated the final cost
+        Dim decSuperHero As Decimal = 380
+        Dim decAutoGraph As Decimal = 275
+        Dim decConvention As Decimal = 209
+
+        'Check if the user enter a numeric value
+        If IsNumeric(txtGroupSize.Text) Then
+            decGroupSize = Convert.ToDecimal(txtGroupSize.Text)
+            'Check if txtGroupSize is greater then zero
+            If decGroupSize > 0 Then
+                'Determine cost per-ticket
+                If radSuperHero.Checked Then
+                    decGroupCost = decSuperHero
+                ElseIf radAutoGraphs.Checked Then
+                    decGroupCost = decAutoGraph
+                ElseIf radConvention.Checked Then
+                    decGroupCost = decConvention
+                End If
+                ' Calculate and display cost
+                decTotalCost = decGroupSize * decGroupCost
+                lblRegistrationCost.Text = " Registration Cost: " & decTotalCost.ToString("C")
+            Else
+                ' Display error message if user enter a negative value
+                MsgBox("You entered " & decGroupSize.ToString() & ". Must be a postive number ", , "Input Error")
+                txtGroupSize.Text = ""
+                txtGroupSize.Focus()
+            End If
+            'Check if group sie is greater then 20
+            If decGroupSize > 20 Then
+                MsgBox("You entered " & decGroupSize.ToString() & ". You have exceeded the number of tickets", , "Input Error")
+            End If
+
+        Else
+            ' Display error message if the user enterd a nonnumeric(Letter) value
+            MsgBox("Enter a number", , "Input Error")
+            txtGroupSize.Text = ""
+            txtGroupSize.Focus()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'This event handler executed when the user click the clear button 
+        'It clear the text group size text box, and set the focus for the text box
+        'and clear the Registration cost label
+
+        txtGroupSize.Clear()
+        lblRegistrationCost.Text = ""
+        radSuperHero.Checked = True
+        radAutoGraphs.Checked = False
+        radConvention.Checked = False
+        txtGroupSize.Focus()
+    End Sub
+
+    Private Sub frmComicConvention_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'This event handler executed when the form loaded at the start of the program.
+        'It set the focus to the GroupSize text box and clear the registration cost label
+
+        lblRegistrationCost.Text = ""
+        txtGroupSize.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'This event handler executed when the user click the Exit button
+        'It close and terminate the program
+        Close()
+    End Sub
+End Class
