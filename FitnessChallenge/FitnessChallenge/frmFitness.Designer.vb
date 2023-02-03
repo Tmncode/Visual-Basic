095cc984bf8ecde9c177707592e1587d80379b5c
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:44:18 -0600

    Add files via upload

---
+++ FitnessChallenge/FitnessChallenge/frmFitness.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFitness
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mnuFitness = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnWeightLoss = New System.Windows.Forms.Button()
+        Me.IstWeightLoss = New System.Windows.Forms.ListBox()
+        Me.lblAverageLoss = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.mnuFitness.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnuFitness
+        '
+        Me.mnuFitness.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.mnuFitness.ImageScalingSize = New System.Drawing.Size(40, 40)
+        Me.mnuFitness.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.mnuFitness.Location = New System.Drawing.Point(0, 0)
+        Me.mnuFitness.Name = "mnuFitness"
+        Me.mnuFitness.Size = New System.Drawing.Size(1553, 49)
+        Me.mnuFitness.TabIndex = 0
+        Me.mnuFitness.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClear, Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(87, 56)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(251, 54)
+        Me.mnuClear.Text = "&Clear"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(251, 54)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'btnWeightLoss
+        '
+        Me.btnWeightLoss.BackColor = System.Drawing.Color.YellowGreen
+        Me.btnWeightLoss.Font = New System.Drawing.Font("Tahoma", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnWeightLoss.Location = New System.Drawing.Point(991, 207)
+        Me.btnWeightLoss.Name = "btnWeightLoss"
+        Me.btnWeightLoss.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.btnWeightLoss.Size = New System.Drawing.Size(388, 75)
+        Me.btnWeightLoss.TabIndex = 2
+        Me.btnWeightLoss.Text = "Enter Weight Loss"
+        Me.btnWeightLoss.UseVisualStyleBackColor = False
+        '
+        'IstWeightLoss
+        '
+        Me.IstWeightLoss.Font = New System.Drawing.Font("Tahoma", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.IstWeightLoss.FormattingEnabled = True
+        Me.IstWeightLoss.ItemHeight = 40
+        Me.IstWeightLoss.Location = New System.Drawing.Point(1122, 298)
+        Me.IstWeightLoss.Name = "IstWeightLoss"
+        Me.IstWeightLoss.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.IstWeightLoss.Size = New System.Drawing.Size(126, 444)
+        Me.IstWeightLoss.TabIndex = 3
+        '
+        'lblAverageLoss
+        '
+        Me.lblAverageLoss.AutoSize = True
+        Me.lblAverageLoss.BackColor = System.Drawing.Color.YellowGreen
+        Me.lblAverageLoss.Font = New System.Drawing.Font("Tahoma", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverageLoss.Location = New System.Drawing.Point(889, 773)
+        Me.lblAverageLoss.Name = "lblAverageLoss"
+        Me.lblAverageLoss.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblAverageLoss.Size = New System.Drawing.Size(548, 41)
+        Me.lblAverageLoss.TabIndex = 4
+        Me.lblAverageLoss.Text = "Average weight loss is XX.X lbs" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.lblAverageLoss.Visible = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.Transparent
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 14.1!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(939, 79)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblTitle.Size = New System.Drawing.Size(466, 116)
+        Me.lblTitle.TabIndex = 5
+        Me.lblTitle.Text = "Fitness Challenge " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Team Weight Loss"
+        '
+        'frmFitness
+        '
+        Me.AcceptButton = Me.btnWeightLoss
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = Global.FitnessChallenge.My.Resources.Resources.health
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1553, 876)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.lblAverageLoss)
+        Me.Controls.Add(Me.IstWeightLoss)
+        Me.Controls.Add(Me.btnWeightLoss)
+        Me.Controls.Add(Me.mnuFitness)
+        Me.MainMenuStrip = Me.mnuFitness
+        Me.Name = "frmFitness"
+        Me.Text = "Fitness Challenge"
+        Me.mnuFitness.ResumeLayout(False)
+        Me.mnuFitness.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents mnuFitness As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents btnWeightLoss As Button
+    Friend WithEvents IstWeightLoss As ListBox
+    Friend WithEvents lblAverageLoss As Label
+    Friend WithEvents lblTitle As Label
+End Class
---
+++ FitnessChallenge/FitnessChallenge/frmFitness.vb
@@ -0,0 +1,84 @@
+﻿'Program Name: Fitness Challenge
+'Author:       Tam M. Nguyen
+'Date:         June 3, 2020
+'Purpose:      The Fitness Challenge program enters the weight loss
+'              loss from team members for a fitness challenge. It displays each 
+'              weight loss value. After all weight loss have been entered,
+'              it displays the average the weight loss for the team.
+
+Option Strict On
+Public Class frmFitness
+    Private Sub btnWeightLoss_Click(sender As Object, e As EventArgs) Handles btnWeightLoss.Click
+        'This event handler executed when the user click the btnWeightLoss it accept up to 8 weight loss
+        'value, and then calculates and displays the average weight loss for the team.
+
+        ' Declare and initialize variables
+        Dim strWeightLoss As String
+        Dim decWeightLoss As Decimal
+        Dim decAverageLoss As Decimal
+        Dim decTotalWeightLoss As Decimal = 0D
+        Dim strInputMessage As String = "Enter weight loss for team member #"
+        Dim strInputHeading As String = "Weight loss"
+        Dim strNormalMessage As String = "Enter the weight loss for team member #"
+        Dim strNonNumericError As String = "Error - Enter the weight loss for team member #"
+        Dim strNegativeError As String = "Error - Enter a positive number for the weight loss of team member #"
+
+        'Declare and initialize loop variables
+        Dim strCancelClicked As String = ""
+        Dim intMaxNumberOfEntries As Integer = 8
+        Dim intNumberOfEntries As Integer = 1
+
+        'This loop allow the user to enter the weight loss of up to 8 team members.
+        'The loop terminates when the user enter 8 weight loss values or the user
+        'click the Cancal Button or click the Close Button in the Inputbox.
+        strWeightLoss = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+
+        Do Until intNumberOfEntries > intMaxNumberOfEntries Or
+            strWeightLoss = strCancelClicked
+            If IsNumeric(strWeightLoss) Then
+                decWeightLoss = Convert.ToDecimal(strWeightLoss)
+                If decWeightLoss > 0 Then
+                    IstWeightLoss.Items.Add(decWeightLoss)
+                    decTotalWeightLoss += decWeightLoss
+                    intNumberOfEntries += 1
+                    strInputMessage = strNormalMessage
+                Else
+                    strInputMessage = strNegativeError
+                End If
+            Else
+                strInputMessage = strNonNumericError
+            End If
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strWeightLoss = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+            End If
+        Loop
+        'Calculate and displays average team weight loss 
+        If intNumberOfEntries > 1 Then
+            lblAverageLoss.Visible = True
+            decAverageLoss = decTotalWeightLoss / (intNumberOfEntries - 1)
+            lblAverageLoss.Text = "Average weight loss is " &
+                decAverageLoss.ToString("F1") & " lbs"
+        Else
+            MsgBox("No weight loss value entered")
+        End If
+
+        'Disables the Weight Loss button
+        btnWeightLoss.Enabled = False
+    End Sub
+
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        'This event handler execute when the user click the Clear button, it clear the list box
+        'and hides the average weight loss label. It also enable the weight loss button
+
+        IstWeightLoss.Items.Clear()
+        lblAverageLoss.Visible = False
+        btnWeightLoss.Enabled = True
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        'This event handler execute when the user click the exit button it terminate
+        'and close the program
+
+        Close()
+    End Sub
+End Class
