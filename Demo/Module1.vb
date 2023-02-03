0e065fb772ac08c76bd336bda0b2edced5672519
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:38:00 -0600

    Add files via upload

---
+++ Demo/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+    'Public Res
+    Sub Main()
+        'Dim x As Integer
+        'x = 10
+        'For i = 0 To x Step 2
+        '    MsgBox("The value of i is : " & i)
+        'Next 'Answer (0, 2, 4, 6, 8, 10)
+
+        'Dim x As String
+        'x = "Miami"
+        'Select Case x
+        '    Case "miami"
+        '        MsgBox("State = Florida")
+        '    Case "richmond"
+        '        MsgBox("State = Virginia")
+        '    Case "dallas"
+        '        MsgBox("State = Texas")
+        'End Select
+
+        'Dim FruitName As String
+        'FruitName = "Apple Mango Grapes"
+        'Res = Split(FruitName, " ")
+        'MsgBox(Res(2)) 'Answer Grapes
+
+
+    End Sub
+End Module
