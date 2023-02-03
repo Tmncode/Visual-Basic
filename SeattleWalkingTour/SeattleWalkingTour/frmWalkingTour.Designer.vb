43ada70f24e66bcdc6d82a1096ba1a21ae4e634b
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:41:44 -0600

    Add files via upload

---
+++ SeattleWalkingTour/SeattleWalkingTour/frmWalkingTour.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWalkingTour
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblCostHeading = New System.Windows.Forms.Label()
+        Me.lblNumberOfTickets = New System.Windows.Forms.Label()
+        Me.lblTicketCost = New System.Windows.Forms.Label()
+        Me.txtNumberOfTickets = New System.Windows.Forms.TextBox()
+        Me.btnDisplayCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.SeattleWalkingTour.My.Resources.Resources.seattle
+        Me.PictureBox1.Location = New System.Drawing.Point(733, 13)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(495, 522)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.1!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.White
+        Me.lblHeading.Location = New System.Drawing.Point(44, 31)
+        Me.lblHeading.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(660, 55)
+        Me.lblHeading.TabIndex = 1
+        Me.lblHeading.Text = "Seattle Walking Tour Tickets" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblCostHeading
+        '
+        Me.lblCostHeading.AutoSize = True
+        Me.lblCostHeading.ForeColor = System.Drawing.Color.White
+        Me.lblCostHeading.Location = New System.Drawing.Point(144, 118)
+        Me.lblCostHeading.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblCostHeading.Name = "lblCostHeading"
+        Me.lblCostHeading.Size = New System.Drawing.Size(0, 39)
+        Me.lblCostHeading.TabIndex = 2
+        '
+        'lblNumberOfTickets
+        '
+        Me.lblNumberOfTickets.AutoSize = True
+        Me.lblNumberOfTickets.ForeColor = System.Drawing.Color.White
+        Me.lblNumberOfTickets.Location = New System.Drawing.Point(47, 227)
+        Me.lblNumberOfTickets.Name = "lblNumberOfTickets"
+        Me.lblNumberOfTickets.Size = New System.Drawing.Size(415, 39)
+        Me.lblNumberOfTickets.TabIndex = 3
+        Me.lblNumberOfTickets.Text = "Enter Number of Tickets:"
+        '
+        'lblTicketCost
+        '
+        Me.lblTicketCost.AutoSize = True
+        Me.lblTicketCost.ForeColor = System.Drawing.Color.White
+        Me.lblTicketCost.Location = New System.Drawing.Point(47, 331)
+        Me.lblTicketCost.Name = "lblTicketCost"
+        Me.lblTicketCost.Size = New System.Drawing.Size(357, 39)
+        Me.lblTicketCost.TabIndex = 4
+        Me.lblTicketCost.Text = "Total Cost of Tickets:"
+        '
+        'txtNumberOfTickets
+        '
+        Me.txtNumberOfTickets.Location = New System.Drawing.Point(500, 224)
+        Me.txtNumberOfTickets.Multiline = True
+        Me.txtNumberOfTickets.Name = "txtNumberOfTickets"
+        Me.txtNumberOfTickets.Size = New System.Drawing.Size(97, 50)
+        Me.txtNumberOfTickets.TabIndex = 5
+        Me.txtNumberOfTickets.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnDisplayCost
+        '
+        Me.btnDisplayCost.BackColor = System.Drawing.Color.White
+        Me.btnDisplayCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.1!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplayCost.Location = New System.Drawing.Point(66, 408)
+        Me.btnDisplayCost.Name = "btnDisplayCost"
+        Me.btnDisplayCost.Size = New System.Drawing.Size(127, 73)
+        Me.btnDisplayCost.TabIndex = 6
+        Me.btnDisplayCost.Text = "Display" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Cost" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnDisplayCost.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.White
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.1!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(300, 408)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(127, 73)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.White
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.1!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(539, 408)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(127, 73)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.BackColor = System.Drawing.Color.White
+        Me.lblTotalCost.Location = New System.Drawing.Point(493, 331)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(0, 39)
+        Me.lblTotalCost.TabIndex = 9
+        '
+        'frmWalkingTour
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(20.0!, 38.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1230, 547)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplayCost)
+        Me.Controls.Add(Me.txtNumberOfTickets)
+        Me.Controls.Add(Me.lblTicketCost)
+        Me.Controls.Add(Me.lblNumberOfTickets)
+        Me.Controls.Add(Me.lblCostHeading)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.900001!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "frmWalkingTour"
+        Me.Text = "Seattle Walking Tour"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblCostHeading As Label
+    Friend WithEvents lblNumberOfTickets As Label
+    Friend WithEvents lblTicketCost As Label
+    Friend WithEvents txtNumberOfTickets As TextBox
+    Friend WithEvents btnDisplayCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblTotalCost As Label
+End Class
---
+++ SeattleWalkingTour/SeattleWalkingTour/frmWalkingTour.vb
@@ -0,0 +1,58 @@
+﻿' Program:     Seattle Walking Tour
+' Author:      Tam M. Nguyen
+' Date:        May 20, 2020
+' Purpose:     This window application compute the cost of seattle walking tour.
+' Program      The user enters the number of tickets need for a three-Hour
+' procedures:  walking tour of the waterfront of seattle. The program calculates and display the cost of the 
+'              tickets with and additional cost of $1.99 for booking using the app
+
+Option Strict On
+
+Public Class frmWalkingTour
+    ' Cost for booking fee
+    Const _cdecBookingFee As Decimal = 1.99D
+    ' Cost per Ticket
+    Const _cdecTickets As Decimal = 14.99D
+
+    Private Sub frmWalkingTour_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' This event handler execute when the form is loaded.
+        ' It display the cost heading, clear the text property of the
+        ' total cost label, and set the focus on
+        ' the text number of ticket text box object.
+
+        lblCostHeading.Text = _cdecTickets.ToString("C") & " per Tickets " & _cdecBookingFee.ToString("C") &
+            " Booking Fee "
+        lblTotalCost.Text = ""
+        txtNumberOfTickets.Focus()
+    End Sub
+
+    Private Sub btnDisplayCost_Click(sender As Object, e As EventArgs) Handles btnDisplayCost.Click
+        ' This event handler execute when the user click the
+        ' Display cost button. It calculate and display the cost
+        ' of the Walking Tour (Number of tickets plus booking fee)
+
+        Dim strNumberOfTickets As String
+        Dim intNumberOfTickets As Integer
+        Dim decTotalCost As Decimal
+
+        strNumberOfTickets = txtNumberOfTickets.Text
+        intNumberOfTickets = Convert.ToInt32(strNumberOfTickets)
+        decTotalCost = intNumberOfTickets * _cdecTickets + _cdecBookingFee
+        lblTotalCost.Text = decTotalCost.ToString("C")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' This event handler clear the Text number of tickets box
+        ' and label total cost text
+
+        txtNumberOfTickets.Clear()
+        lblTotalCost.Text = ""
+        txtNumberOfTickets.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' This event handler close the window and terminate the application
+
+        Close()
+    End Sub
+End Class
