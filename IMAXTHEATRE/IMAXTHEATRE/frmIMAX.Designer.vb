d7b36eeba1de58cdf56fd831f5a5262cfb95aa2e
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:46:53 -0600

    Add files via upload

---
+++ IMAXTHEATRE/IMAXTHEATRE/frmIMAX.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmIMAX
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlImage = New System.Windows.Forms.Panel()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblTickets = New System.Windows.Forms.Label()
+        Me.cboTicketChoice = New System.Windows.Forms.ComboBox()
+        Me.txtTickets = New System.Windows.Forms.TextBox()
+        Me.btnCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'pnlImage
+        '
+        Me.pnlImage.BackgroundImage = Global.IMAXTHEATRE.My.Resources.Resources.IMAX
+        Me.pnlImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pnlImage.Location = New System.Drawing.Point(1, 0)
+        Me.pnlImage.Name = "pnlImage"
+        Me.pnlImage.Size = New System.Drawing.Size(400, 450)
+        Me.pnlImage.TabIndex = 0
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.RoyalBlue
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(457, 21)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(295, 33)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "IMAX Theatre Tickets"
+        '
+        'lblTickets
+        '
+        Me.lblTickets.AutoSize = True
+        Me.lblTickets.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTickets.Location = New System.Drawing.Point(484, 133)
+        Me.lblTickets.Name = "lblTickets"
+        Me.lblTickets.Size = New System.Drawing.Size(192, 25)
+        Me.lblTickets.TabIndex = 2
+        Me.lblTickets.Text = "Number of Tickets:"
+        Me.lblTickets.Visible = False
+        '
+        'cboTicketChoice
+        '
+        Me.cboTicketChoice.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboTicketChoice.FormattingEnabled = True
+        Me.cboTicketChoice.Items.AddRange(New Object() {"Matinee $16", "Evening $27"})
+        Me.cboTicketChoice.Location = New System.Drawing.Point(518, 73)
+        Me.cboTicketChoice.Name = "cboTicketChoice"
+        Me.cboTicketChoice.Size = New System.Drawing.Size(169, 33)
+        Me.cboTicketChoice.TabIndex = 3
+        Me.cboTicketChoice.Text = "Select Tickets"
+        '
+        'txtTickets
+        '
+        Me.txtTickets.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTickets.Location = New System.Drawing.Point(682, 130)
+        Me.txtTickets.Name = "txtTickets"
+        Me.txtTickets.Size = New System.Drawing.Size(37, 31)
+        Me.txtTickets.TabIndex = 4
+        Me.txtTickets.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtTickets.Visible = False
+        '
+        'btnCost
+        '
+        Me.btnCost.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCost.ForeColor = System.Drawing.Color.White
+        Me.btnCost.Location = New System.Drawing.Point(552, 188)
+        Me.btnCost.Name = "btnCost"
+        Me.btnCost.Size = New System.Drawing.Size(124, 37)
+        Me.btnCost.TabIndex = 5
+        Me.btnCost.Text = "Ticket Cost"
+        Me.btnCost.UseVisualStyleBackColor = False
+        Me.btnCost.Visible = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(552, 244)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(124, 37)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear Form"
+        Me.btnClear.UseVisualStyleBackColor = False
+        Me.btnClear.Visible = False
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.BackColor = System.Drawing.Color.RoyalBlue
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.ForeColor = System.Drawing.Color.White
+        Me.lblTotal.Location = New System.Drawing.Point(514, 314)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(141, 20)
+        Me.lblTotal.TabIndex = 7
+        Me.lblTotal.Text = "XXXXXXXXXXXX"
+        Me.lblTotal.Visible = False
+        '
+        'frmIMAX
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 377)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCost)
+        Me.Controls.Add(Me.txtTickets)
+        Me.Controls.Add(Me.cboTicketChoice)
+        Me.Controls.Add(Me.lblTickets)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.pnlImage)
+        Me.Name = "frmIMAX"
+        Me.Text = "IMAX THEATRE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pnlImage As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblTickets As Label
+    Friend WithEvents cboTicketChoice As ComboBox
+    Friend WithEvents txtTickets As TextBox
+    Friend WithEvents btnCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblTotal As Label
+End Class
---
+++ IMAXTHEATRE/IMAXTHEATRE/frmIMAX.vb
@@ -0,0 +1,144 @@
+﻿' Program Name:  IMAX THEATRE
+' Author:        Tam M. Nguyen
+' Date:          August 20, 2020
+' Purpose:       The IMAX application computes the cost of matinee and evening tickets with varyimg base and data plans
+
+Option Strict On
+
+Public Class frmIMAX
+
+    Private Sub cboTicketChoice_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboTicketChoice.SelectedIndexChanged
+        ' This event handler allows the user to enter there Imax choice and then calls subprocedures to place the Imax type
+        Dim intTicketChoice As Integer
+
+        intTicketChoice = cboTicketChoice.SelectedIndex
+
+        ' Make items visible in the window
+        lblTickets.Visible = True
+        txtTickets.Visible = True
+        btnCost.Visible = True
+        btnClear.Visible = True
+        lblTotal.Visible = True
+        ' Clear label 
+        lblTotal.Text = ""
+        ' set focus on the number in textbox
+        txtTickets.Focus()
+    End Sub
+
+    Private Sub btnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
+        ' This button event handler determines the cost of IMAX tickets choice and display the total cost
+        Dim intTicketNumber As Integer
+        Dim blnNumberOfTicketIsValid As Boolean = False
+        Dim blnImaxIsSelected As Boolean = False
+        Dim intImaxChoice As Integer
+        Dim intTicketChoice As Integer
+        Dim strSelectedTicket As String = ""
+        Dim decTotalCost As Decimal
+
+        ' Call a function to ensure the number of ticket is valid 
+        blnNumberOfTicketIsValid = ValidateNumberOfTicket()
+        ' call a function to ensure a Imax was selected
+        intImaxChoice = ValidateImaxSelection(blnImaxIsSelected, strSelectedTicket)
+        If (blnNumberOfTicketIsValid And blnImaxIsSelected) Then
+            intTicketNumber = Convert.ToInt32(txtTickets.Text)
+            intTicketChoice = cboTicketChoice.SelectedIndex
+            Select Case intTicketChoice
+                Case 0
+                    decTotalCost = MatineeImaxCost(intImaxChoice, intTicketNumber)
+                Case 1
+                    decTotalCost = EveningImaxCost(intImaxChoice, intTicketNumber)
+            End Select
+            ' Display the cost
+            lblTotal.Text = "Cost: " & decTotalCost.ToString("C") & " for the tickets"
+        End If
+    End Sub
+
+    Private Function ValidateNumberOfTicket() As Boolean
+        ' This procedure validates the value entered for the text Ticket box
+        Dim intTicketNumber As Integer
+        Dim blnValidityCheck As Boolean = False
+        Dim strNumberOfTicketMessage As String = "Please enter the number of tickets for Imax Theatre"
+        Dim strMessageBoxTitle As String = "Error"
+
+        Try
+            intTicketNumber = Convert.ToInt32(txtTickets.Text)
+            If intTicketNumber >= 0 And intTicketNumber <= 10 Then
+                blnValidityCheck = True
+            Else
+                MsgBox(strNumberOfTicketMessage, , strMessageBoxTitle)
+                txtTickets.Focus()
+                txtTickets.Clear()
+            End If
+        Catch Exception As FormatException
+            MsgBox(strNumberOfTicketMessage, , strMessageBoxTitle)
+            txtTickets.Focus()
+            txtTickets.Clear()
+        Catch Exception As OverflowException
+            MsgBox(strNumberOfTicketMessage, , strMessageBoxTitle)
+            txtTickets.Focus()
+            txtTickets.Clear()
+        Catch Exception As SystemException
+            MsgBox(strNumberOfTicketMessage, , strMessageBoxTitle)
+            txtTickets.Focus()
+            txtTickets.Clear()
+        End Try
+        Return blnValidityCheck
+    End Function
+
+    Private Function ValidateImaxSelection(ByRef blnImax As Boolean, ByRef strImax As String) As Integer
+        ' This function ensures the user selected a Imax type
+        Dim intImaxType As Integer
+        Try
+            intImaxType = Convert.ToInt32(cboTicketChoice.SelectedIndex)
+            strImax = cboTicketChoice.SelectedItem.ToString()
+            blnImax = True
+        Catch Exception As SystemException
+            ' Determine if the Imax type is selected
+            MsgBox("Select a Imax type", , "Error")
+            blnImax = False
+        End Try
+        Return intImaxType
+    End Function
+    Private Function MatineeImaxCost(ByVal intImax As Integer, ByVal intNumber As Integer) As Decimal
+        ' This function calculates the cost for Imax matinee
+        Dim decTicketCost As Decimal
+        Dim decFinalCost As Decimal
+        Dim decMatineeImax As Decimal = 16D
+
+        Select Case intImax
+            Case 0
+                decTicketCost = decMatineeImax
+        End Select
+        decFinalCost = decTicketCost * intNumber
+        Return decFinalCost
+    End Function
+    Private Function EveningImaxCost(ByVal intImax As Integer, ByVal intNumber As Integer) As Decimal
+        ' This function calculates the cost for Imax evening
+        Dim decTicketCost As Decimal
+        Dim decFinalCost As Decimal
+        Dim decEveningImax As Decimal = 27D
+
+        Select Case intImax
+            Case 1
+                decTicketCost = decEveningImax
+        End Select
+        decFinalCost = decTicketCost * intNumber
+        Return decFinalCost
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' This event handler clears the form for reuse when user clicks the clear button
+        cboTicketChoice.Text = "Select Tickets"
+        txtTickets.Clear()
+        lblTotal.Text = ""
+        lblTickets.Visible = False
+        txtTickets.Visible = False
+        btnClear.Visible = False
+        lblTotal.Visible = False
+    End Sub
+
+    Private Sub frmIMAX_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Hold the splash screen for 4 seconds
+        Threading.Thread.Sleep(2000)
+    End Sub
+End Class
