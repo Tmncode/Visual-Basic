07f6ad820c7fcd1c62fe15d0dc8f3a4114b5c3a8
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:48:02 -0600

    Add files via upload

---
+++ Depreciation/Depreciation/frmDispalyInventory.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDisplayInventory
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReturn = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.LblTitle2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnReturn
+        '
+        Me.btnReturn.BackColor = System.Drawing.Color.OrangeRed
+        Me.btnReturn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReturn.ForeColor = System.Drawing.Color.White
+        Me.btnReturn.Location = New System.Drawing.Point(92, 297)
+        Me.btnReturn.Name = "btnReturn"
+        Me.btnReturn.Size = New System.Drawing.Size(187, 44)
+        Me.btnReturn.TabIndex = 0
+        Me.btnReturn.Text = "Return to Application"
+        Me.btnReturn.UseVisualStyleBackColor = False
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.lstDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.ForeColor = System.Drawing.Color.Gray
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 20
+        Me.lstDisplay.Location = New System.Drawing.Point(92, 88)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(187, 164)
+        Me.lstDisplay.TabIndex = 1
+        '
+        'LblTitle2
+        '
+        Me.LblTitle2.AutoSize = True
+        Me.LblTitle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 17.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTitle2.ForeColor = System.Drawing.Color.OrangeRed
+        Me.LblTitle2.Location = New System.Drawing.Point(87, 18)
+        Me.LblTitle2.Name = "LblTitle2"
+        Me.LblTitle2.Size = New System.Drawing.Size(257, 29)
+        Me.LblTitle2.TabIndex = 2
+        Me.LblTitle2.Text = "Chef Shack Inventory"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Depreciation.My.Resources.Resources.Truck
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(79, 59)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'frmDisplayInventory
+        '
+        Me.AcceptButton = Me.btnReturn
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(371, 380)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.LblTitle2)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.btnReturn)
+        Me.Name = "frmDisplayInventory"
+        Me.Text = "Sorted Inventory Listing"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnReturn As Button
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents LblTitle2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Depreciation/Depreciation/frmDispalyInventory.vb
@@ -0,0 +1,28 @@
+﻿' The frmDisplayInventory class is opened by frmDepreciation
+' and displays the Inventory file in sorted order.
+
+Option Strict On
+
+Public Class frmDisplayInventory
+    Private Sub frmDisplayInventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' This frmDisplayInventory load event is the second forms that
+        ' displays the sorted inventory items
+
+        Dim strItem As String
+        ' sorts the _strInventoryItem Array
+        Array.Sort(frmDepreciation._strInventoryItem)
+
+        ' Displays the _strInventoryItem array
+        For Each strItem In frmDepreciation._strInventoryItem
+            lstDisplay.Items.Add(strItem)
+        Next
+    End Sub
+
+    Private Sub btnReturn_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
+        ' The sub procedure opens the first form
+        Dim frmFirst As New frmDepreciation
+
+        Hide()
+        frmFirst.ShowDialog()
+    End Sub
+End Class
